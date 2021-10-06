7f3e3d981ac324b2bc43d7f958cbada56d963a12
Andy Gao <andygao1990@gmail.com>
2021-10-05 20:49:59 -0400

    added db

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,10 @@
 const { expect } = require('chai')
+const { syncAndSeed } = require('../db')
 
 const app = require('supertest')(require('../app'))
 
 describe ('Routes', () => {
+    beforeEach(()=>syncAndSeed())
     describe('GET /', () => {
         it('show information about the api', async ()=> {
             const response = await app.get('/')
@@ -10,4 +12,12 @@ describe ('Routes', () => {
             expect(response.text).to.include('The Acme Movie API')
         })
     })
+    describe('GET /api', () => {
+        it('show information about the api', async ()=> {
+            const response = await app.get('/')
+            expect(response.status).to.equal(200)
+            expect(response.text).to.include('The Acme Movie API')
+        })
+    })
+
 })
