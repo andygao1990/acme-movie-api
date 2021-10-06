3fdebe4c68597302e14fd540f5b632901e04d64f
Andy Gao <andygao1990@gmail.com>
2021-10-05 20:18:54 -0400

    commit

--- test/spec.js
+++ test/spec.js
@@ -3,7 +3,7 @@ const { expect } = require('chai')
 describe ('Route', () => {
     describe('GET /', () => {
         it('show information about the api', ()=> {
-            
+            expect('hello').to.equal('hello')
         })
     })
 })
